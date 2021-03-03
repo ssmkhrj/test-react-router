8707f60a0c9912b8385b47b0ee33b6a4ac5f22bb
ssmkhrj <iamssmkhrj@gmail.com>
2021-03-03 21:35:00 +0530

    V1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  Route,
+} from "react-router-dom";
 
-function App() {
+const Home = ({ setLevel }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Choose Difficulty</h1>
+      <ul>
+        <li onClick={setLevel("easy")}>Easy</li>
+        <li onClick={setLevel("hard")}>Hard</li>
+      </ul>
+      <Link to="/game">Play</Link>
+    </>
   );
-}
+};
+
+const Game = ({ level }) => {
+  return (
+    <>
+      <h1>Welcome to the game</h1>
+      <p>Your level is {level}</p>
+      <Link to="/score">Score</Link>
+      <br />
+      <Link to="/user/10">User 10</Link>
+    </>
+  );
+};
+
+const Score = () => {
+  return (
+    <>
+      <Link to="/game">Back</Link>
+      <h1>Score Page</h1>
+    </>
+  );
+};
+
+const User = () => {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/score">Score</Link>
+      <br />
+      <Link to="/game">Game</Link>
+      <br />
+      <Link to="/user/21">User 21</Link>
+      <br />
+    </>
+  );
+};
+
+const App = () => {
+  const [level, setLevel] = useState(null);
+  return (
+    <Router>
+      <Route path="/">
+        <Redirect to="/" />
+      </Route>
+      <Route exact path="/" render={() => <Home setLevel={setLevel} />}></Route>
+      <Route exact path="/game" render={() => <Game level={level} />}></Route>
+      <Route exact path="/score" render={() => <Score />}></Route>
+      <Route exact path="/user/:userid" render={() => <User />}></Route>
+    </Router>
+  );
+};
 
 export default App;
